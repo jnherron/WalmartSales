eb6ebe7ce3b3f3bf93364f12f3aeb6a5c7d4c9bc
Jamie Herron <jnherron3@gmail.com>
2025-11-23 00:21:47 -0700

    Performed models
    Created recipe and performed penalized regression, random forest, boost, and facebook prophet
    Updated README

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 Walmart Recruiting - Store Sales Forecasting Kaggle Competition
 
 WalmartAnalysis.R
-- Uploads data and joins various tables into two data sets: train_data and test_data
+- Uploads data
+    - Joins various tables into two data sets: train_data and test_data
+    - Removes NA's in data frames
 - Performs some basic EDA
+- Creates recipe
+- Performs Penalized Regression
+- Performs Random Forest
+- Performs Boost
+- Performs Facebook Prophet
 
+Scrap.R
+- A file I copied code to while testing various models
--- WalmartAnalysis.R
+++ WalmartAnalysis.R
@@ -1,10 +1,19 @@
 library(vroom)
 library(dplyr)
-library(DataExplorer)
+#library(DataExplorer)
+library(tidyverse)
+library(tidymodels)
+library(glmnet)
+library(rpart)
+library(ranger)
+library(bonsai)
+library(lightgbm)
+library(agua)
+library(prophet)
 
 
 
-
+#
 # Upload Data ------------------------------------------------------------------
 
 train_data <- vroom("train.csv.zip")
@@ -13,21 +22,24 @@ stores_data <- vroom("stores.csv")
 features_data <- vroom("features.csv.zip")
 
 # Join Datasets
+train_data <- left_join(train_data, features_data, by = c("Store", "Date", "IsHoliday"))
 train_data <- left_join(train_data, stores_data, by = "Store")
 
-train_data$Store_Date <- paste0(as.character(train_data$Store), "_", as.character(train_data$Date))
-features_data$Store_Date <- paste0(as.character(features_data$Store), "_", as.character(features_data$Date))
-features_data$Store <- NULL
-features_data$Date <- NULL
-features_data$IsHoliday <- NULL
-train_data <- left_join(train_data, features_data, by = "Store_Date")
+test_data <- left_join(test_data, features_data, by = c("Store", "Date", "IsHoliday"))
+test_data <- left_join(test_data, stores_data, by = "Store")
 
-# Get rid of NA's
+# Get rid of MarkDown NA's
 train_data$MarkDown1[is.na(train_data$MarkDown1)] <- 0
 train_data$MarkDown2[is.na(train_data$MarkDown2)] <- 0
 train_data$MarkDown3[is.na(train_data$MarkDown3)] <- 0
 train_data$MarkDown4[is.na(train_data$MarkDown4)] <- 0
 train_data$MarkDown5[is.na(train_data$MarkDown5)] <- 0
+
+test_data$MarkDown1[is.na(test_data$MarkDown1)] <- 0
+test_data$MarkDown2[is.na(test_data$MarkDown2)] <- 0
+test_data$MarkDown3[is.na(test_data$MarkDown3)] <- 0
+test_data$MarkDown4[is.na(test_data$MarkDown4)] <- 0
+test_data$MarkDown5[is.na(test_data$MarkDown5)] <- 0
 
 # More MarkDown Wrangling
 train_data$TotalMarkDown <- train_data$MarkDown1 + train_data$MarkDown2 + train_data$MarkDown3 + train_data$MarkDown4 + train_data$MarkDown5
@@ -38,46 +50,143 @@ train_data$MarkDown3 <- NULL
 train_data$MarkDown4 <- NULL
 train_data$MarkDown5 <- NULL
 
+test_data$TotalMarkDown <- test_data$MarkDown1 + test_data$MarkDown2 + test_data$MarkDown3 + test_data$MarkDown4 + test_data$MarkDown5
+test_data$MarkDownFlag <- ifelse(test_data$TotalMarkDown != 0, TRUE, FALSE)
+test_data$MarkDown1 <- NULL
+test_data$MarkDown2 <- NULL
+test_data$MarkDown3 <- NULL
+test_data$MarkDown4 <- NULL
+test_data$MarkDown5 <- NULL
+
+# Get rid of other NA's
+feature_recipe <- recipe(~., data=train_data) %>%
+  step_mutate(DecDate=decimal_date(Date)) %>%
+  step_impute_bag(CPI, Unemployment, impute_with=imp_vars(DecDate, Store))
+train_data <- juice(prep(feature_recipe))
+
+feature_recipe <- recipe(~., data=test_data) %>%
+  step_mutate(DecDate=decimal_date(Date)) %>%
+  step_impute_bag(CPI, Unemployment, impute_with=imp_vars(DecDate, Store))
+test_data <- juice(prep(feature_recipe))
 
 
 # EDA --------------------------------------------------------------------------
 
-DataExplorer::plot_intro(train_data)
-DataExplorer::plot_correlation(train_data)
-DataExplorer::plot_bar(train_data)
-DataExplorer::plot_missing(train_data)
-
+# DataExplorer::plot_intro(train_data)
+# DataExplorer::plot_correlation(train_data)
+# DataExplorer::plot_bar(train_data)
+# DataExplorer::plot_missing(train_data)
 
 # Recipe -----------------------------------------------------------------------
 
-# recipe
-my_recipe <- recipe(count~., data=train_data) %>%
-  step_mutate(weather = ifelse(weather == 4,3,weather)) %>%
-  step_mutate(weather=factor(weather)) %>%
-  step_time(datetime, features=c("hour")) %>%
-  step_date(datetime, features=c("dow", "month", "year")) %>%
-  step_mutate(datetime_dow = as.numeric(datetime_dow)) %>%
-  step_harmonic(datetime_hour, cycle_size = 24, frequency = 1) %>%
-  step_harmonic(datetime_dow, cycle_size = 7, frequency = 1) %>%
-  step_rm(datetime) %>%
-  step_mutate(season=factor(season)) %>%
-  step_mutate(workingday=factor(workingday)) %>%
-  step_mutate(holiday=factor(holiday)) %>%
-  # step_interact(~ datetime_hour:workingday) %>%
-  # step_interact(~ holiday:workingday) %>%
-  # step_interact(~ weather:workingday) %>%
-  # step_interact(~ season:workingday) %>%
+# Filter by Store and Department
+train_data <- train_data[train_data$Store == 17,]  # 25
+train_data <- train_data[train_data$Dept == 17,] # 11
+
+my_recipe <- recipe(Weekly_Sales~., data=train_data) %>%
+  step_date(Date, features=c("doy", "month", "year")) %>%
+  step_range(Date_doy, min=0, max=pi) %>%
+  step_mutate(sinDOY=sin(Date_doy), cosDOY=cos(Date_doy)) %>%
+  step_rm(Date) %>%
+  step_mutate(IsHoliday = factor(IsHoliday), MarkDownFlag = factor(MarkDownFlag)) %>%
   step_dummy(all_nominal_predictors()) %>%
   step_normalize(all_numeric_predictors())
 
 
 
+# Penalized Regression ---------------------------------------------------------
+
+# preg_model <- linear_reg(penalty=tune(), mixture=tune()) %>%
+#   set_engine("glmnet")
+# 
+# preg_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(preg_model)
+# 
+# tuning_grid <- grid_regular(penalty(), mixture(), levels=3)
+# folds <- vfold_cv(train_data, v=3, repeats=1)
+# 
+# cv_results <- preg_wf %>%
+#   tune_grid(resamples=folds, grid=tuning_grid, metrics=metric_set(mae))
+# best_tune <- cv_results %>%
+#   collect_metrics()
+# 
+# mean(best_tune$std_err)
 
 
+# Random Forests ---------------------------------------------------------------
 
-# # apply the recipe to your data1
-# prep <- prep(my_recipe)
-# baked <- bake(prep, new_data = train_data)
+# forest_mod <- rand_forest(mtry=tune(), min_n=tune(), trees=tune()) %>%
+#   set_engine("ranger") %>%
+#   set_mode("regression")
+# 
+# forest_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(forest_mod)
+# 
+# tuning_grid <- grid_regular(mtry(range=c(1,10)), min_n(), levels=3)
+# folds <- vfold_cv(train_data, v=3, repeats=1)
+# 
+# cv_results <- forest_wf %>%
+#   tune_grid(resamples=folds, grid=tuning_grid, metrics=metric_set(mae))
+# best_tune <- cv_results %>%
+#   collect_metrics()
+# 
+# mean(best_tune$std_err)
+
+
+# Boost ------------------------------------------------------------------------
+
+# boost_mod <- boost_tree(tree_depth=tune(), trees=tune(), learn_rate=tune()) %>%
+#   set_engine("lightgbm") %>%
+#   set_mode("regression")
+# 
+# boost_wf <- workflow() %>%
+#   add_recipe(my_recipe) %>%
+#   add_model(boost_mod)
+# 
+# tuning_grid <- grid_regular(tree_depth(), trees(), learn_rate(), levels=3)
+# folds <- vfold_cv(train_data, v=3, repeats=1)
+# 
+# cv_results <- boost_wf %>%
+#   tune_grid(resamples=folds, grid=tuning_grid, metrics=metric_set(mae))
+# best_tune <- cv_results %>%
+#   collect_metrics()
+# 
+# mean(best_tune$std_err)
+
+
+# Facebook Prophet -------------------------------------------------------------
+
+# Need to have the column names right for prophet
+prophet_data <- train_data %>%
+  rename(y=Weekly_Sales, ds=Date)
+
+# Filter and Rename to match prophet syntax
+sd_train <- train_data %>%
+  #filter(Store==store, Dept==dept) %>%
+  rename(y=Weekly_Sales, ds=Date)
+sd_test <- test_data %>%
+  #filter(Store==store, Dept==dept) %>%
+  rename(ds=Date)
+
+## Fit a prophet model
+prophet_model <- prophet() %>%
+  add_regressor("CPI") %>%
+  fit.prophet(df=sd_train)
+
+# Predict Using Fitted prophet Model
+fitted_vals <- predict(prophet_model, df=sd_train)   # For Plotting Fitted Values
+test_preds <- predict(prophet_model, df=sd_test)     # Predictions are called "yhat"
+
+## Plot Fitted and Forecast on Same Plot
+plot2 <- ggplot() +
+  geom_line(data = sd_train, mapping = aes(x = ds, y = y, color = "Data")) +
+  geom_line(data = fitted_vals, mapping = aes(x = as.Date(ds), y = yhat, color = "Fitted")) +
+  geom_line(data = test_preds, mapping = aes(x = as.Date(ds), y = yhat, color = "Forecast")) +
+  scale_color_manual(values = c("Data" = "black", "Fitted" = "blue", "Forecast" = "red")) +
+  labs(color="")
+
 
 
 
